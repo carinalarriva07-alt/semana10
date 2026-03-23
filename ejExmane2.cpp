7499449efa5b1fc7abb57dffc4de3bc48b8743ea
carinalarriva <carinalarriva@gmail.com>
2026-03-23 14:52:08 -0500

    examen

--- ejExmane2.cpp
+++ ejExmane2.cpp
@@ -12,9 +12,9 @@ int main() {
     cout << "Ingrese un numero : " << endl;
     cin >> n;
 
-    for ( int i=0; i < 4; i++) {  // Errores: el for debe inicializarse en 1
-        for ( int j=0; j < n; j++) {  // Errores: el for debe inicializarse en 1 y la condicion debe ser j <= n
-            cout << i << → << j << endl; // Errores: se debe poner "" en lugar de → y se debe imprmir
+    for ( int i=0; i < 4; i++) {  // Error: utilize 2 for lo que hacia que se repita el bucle mas veces de lo necesario, se debe utilizar un solo for para imprimir los numeros del 1 al n con su cuadrado.
+        for ( int j=0; j < n; j++) 
+            cout << i << → << j << " "; // Errores: se debe poner "" en lugar de solo → y se debe imprimir la multiplicacion (el cuadrado) en lugar de la j.
 
         }
     }
@@ -36,7 +36,7 @@ int main() {
 
 
     int n = 0;
-    int multiplicacion = 0;
+  
 
     cout << "Ingrese un numero : " << endl;
     cin >> n;
